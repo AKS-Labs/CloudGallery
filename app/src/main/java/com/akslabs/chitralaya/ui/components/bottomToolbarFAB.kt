05bd0b092a7269c5b69cb352a1f7336184cc17bb
AKS Labs <adityashinde.info@gmail.com>
2025-11-13 12:33:04 +0530

    added shadows and elevation to bottom toolbar

--- app/src/main/java/com/akslabs/chitralaya/ui/components/bottomToolbarFAB.kt
+++ app/src/main/java/com/akslabs/chitralaya/ui/components/bottomToolbarFAB.kt
@@ -2,55 +2,41 @@ package com.akslabs.chitralaya.ui.components
 
 import android.net.http.SslCertificate.restoreState
 import android.net.http.SslCertificate.saveState
+import androidx.compose.animation.core.LinearEasing
+import androidx.compose.animation.core.RepeatMode
+import androidx.compose.animation.core.animateFloat
+import androidx.compose.animation.core.infiniteRepeatable
+import androidx.compose.animation.core.rememberInfiniteTransition
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.ArrowUpward
-import androidx.compose.material.icons.filled.Check
 import androidx.compose.material.icons.filled.Cloud
-import androidx.compose.material.icons.filled.Download
-import androidx.compose.animation.core.*
-import androidx.compose.foundation.isSystemInDarkTheme
-import androidx.compose.foundation.layout.offset
-import androidx.compose.material.icons.filled.ArrowUpward
-import androidx.compose.material3.FilledIconButton
-import androidx.compose.material3.HorizontalFloatingToolbar
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
-import androidx.compose.runtime.*
-import androidx.compose.ui.draw.alpha
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.zIndex
-
-import androidx.compose.runtime.getValue
-import androidx.navigation.compose.currentBackStackEntryAsState
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.ui.graphics.Color
-
-
-import androidx.compose.material.icons.filled.Edit
-import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.material.icons.filled.PhoneAndroid
 import androidx.compose.material3.ExperimentalMaterial3ExpressiveApi
 import androidx.compose.material3.FilledIconButton
-import androidx.compose.material3.FloatingToolbarDefaults.ScreenOffset
+import androidx.compose.material3.FloatingToolbarDefaults
 import androidx.compose.material3.HorizontalFloatingToolbar
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.zIndex
 import androidx.navigation.NavHostController
-
+import androidx.navigation.compose.currentBackStackEntryAsState
 import com.akslabs.cloudgallery.ui.main.nav.Screens
 
 @OptIn(ExperimentalMaterial3ExpressiveApi::class)
@@ -61,9 +47,7 @@ fun BottomToolbarFAB(
     navController: NavHostController,
     modifier: Modifier = Modifier,
 ) {
-    var fabState by remember { mutableStateOf(FabState.Inactive) }
     var isUploading by remember { mutableStateOf(false) }
-
 
     val transition = rememberInfiniteTransition(label = "")
     val offsetY by transition.animateFloat(
@@ -87,15 +71,16 @@ fun BottomToolbarFAB(
 
     HorizontalFloatingToolbar(
         modifier = modifier
-            .offset(y = -ScreenOffset)
+            .offset(y = -FloatingToolbarDefaults.ScreenOffset)
+            .shadow(6.dp, FloatingToolbarDefaults.ContainerShape)
+            .clip(FloatingToolbarDefaults.ContainerShape)
             .zIndex(1f),
         expanded = expanded,
         leadingContent = { LeadingContent(navController) },
         trailingContent = { TrailingContent(navController) },
         content = {
-
             FilledIconButton(
-                modifier = Modifier.width(64.dp),
+                modifier = Modifier.width(66.dp),
                 onClick = { isUploading = !isUploading }
             ) {
                 if (isUploading) {
@@ -104,7 +89,6 @@ fun BottomToolbarFAB(
                         contentDescription = "Uploading",
                         modifier = Modifier
                             .offset(y = offsetY.dp)
-//                            .alpha(alpha)
                     )
                 } else {
                     Icon(
@@ -112,9 +96,7 @@ fun BottomToolbarFAB(
                         contentDescription = "Upload"
                     )
                 }
-//                Icon(Icons.Filled.ArrowUpward, contentDescription = "Localized description")
             }
-
         },
     )
 }
@@ -148,7 +130,6 @@ private fun LeadingContent(navController: NavHostController) {
 
     Spacer(modifier = Modifier.size(18.dp))
 }
-
 
 @Composable
 private fun TrailingContent(navController: NavHostController) {
