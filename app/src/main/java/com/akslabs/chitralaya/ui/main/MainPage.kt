21c11f26fc1211aaccf6528db852078fb0cbe2cc
AKS Labs <adityashinde.info@gmail.com>
2025-11-21 16:31:37 +0530

    added expanded height to topappbar

--- app/src/main/java/com/akslabs/chitralaya/ui/main/MainPage.kt
+++ app/src/main/java/com/akslabs/chitralaya/ui/main/MainPage.kt
@@ -18,6 +18,7 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.navigationBars
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
@@ -279,7 +280,7 @@ fun MainPage(viewModel: MainViewModel = screenScopedViewModel()) {
 
                                                 )
                                             }
-                                        },
+                                        },expandedHeight = 90.dp,
                                         actions = {
                                             Row(
                                                 modifier = Modifier.padding(top = 30.dp)
@@ -487,7 +488,7 @@ fun SelectionTopAppBar(
                         contentAlignment = Alignment.Center
                     ) {
                         Text("$selectedCount Selected  ",
-                        fontWeight = FontWeight.Bold)
+                        fontWeight = FontWeight.SemiBold)
                     }
                 }
             )
